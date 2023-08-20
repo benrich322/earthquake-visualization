0ef94c2ce90adee705fd8688507dc5293e5b2098
Ben Richardson <benrichardson@bens-air.lan>
2023-08-19 22:38:51 -0500

    starter files and leaflet

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,4 +9,22 @@ let myMap = L.map("map", {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
 
-  
+// Load GeoJSON data using an AJAX request
+fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
+.then(response => response.json())
+.then(data => {
+    // Create a GeoJSON layer and add it to the map
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: feature.properties.mag * 3, // Customize marker size
+                fillColor: 'red',
+                color: 'red',
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            }).bindPopup('Magnitude: ' + feature.properties.mag + '<br>Location: ' + feature.properties.place);
+        }
+    }).addTo(myMap);
+})
+.catch(error => console.error('Error loading GeoJSON data:', error));
