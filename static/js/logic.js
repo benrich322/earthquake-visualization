04040a33c9b659b167a98b7fa5d010790179e256
Ben Richardson <benrichardson@bens-air.lan>
2023-08-19 22:31:16 -0500

    added leaflet

---
+++ static/js/logic.js
@@ -0,0 +1,12 @@
+// Creating the map object
+let myMap = L.map("map", {
+    center: [40, -111],
+    zoom: 5
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
+  
